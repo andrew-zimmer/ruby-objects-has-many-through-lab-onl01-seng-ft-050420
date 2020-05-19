3915f048527d4f682f9c61b974d87b45582addb9
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-19 13:53:14 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,7 +1,14 @@
 class Doctor 
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
-    @name = name 
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @all 
   end 
 end 
